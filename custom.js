8935a90e863b2f7d754ddf5bdf4c10da6d94b79b
jeelani Bhat <jeel.scorpion007@gmail.com>
2018-08-13 11:23:32 +0530

    mobile nav with addition of customNavbar.js

--- custom.js
+++ custom.js
@@ -98,3 +98,32 @@ $('#btn-new-address').click(function() {
     $('#new-address').slideDown();
 });
 
+// mobile menu 
+$(document).ready(function () {
+    var trigger = $('.hamburger'),
+        overlay = $('.overlay'),
+       isClosed = false;
+
+    function buttonSwitch() {
+
+        if (isClosed === true) {
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+        } else {
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+        }
+    }
+
+    trigger.click(function () {
+        buttonSwitch();
+    });
+
+    $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+    });
+});
